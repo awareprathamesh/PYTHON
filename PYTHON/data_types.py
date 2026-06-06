3297a454c90ff70e8fcb45277afc57449cdaa9fe
awareprathamesh <prathameshaware58@gmail.com>
2026-06-06 10:53:51 +0530

    Update data_types.py

--- PYTHON/data_types.py
+++ PYTHON/data_types.py
@@ -1,1 +1,13 @@
+name = "Prathamesh"      # String
+age = 21                # Integer
+percentage = 55.5       # Float
+is_student = True       # Boolean
+marks = [80, 90, 95]    # List
+data = None             # NoneType
 
+print("name:", type(name))
+print("age:", type(age))
+print("percentage:", type(percentage))
+print("is_student:", type(is_student))
+print("marks:", type(marks))
+print("data:", type(data))
