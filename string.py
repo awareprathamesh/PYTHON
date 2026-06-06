ea449c0970ae52722649b3dfd4757dbe2c1773a6
awareprathamesh <prathameshaware58@gmail.com>
2026-06-06 15:36:36 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,1 +1,15 @@
+#THIS IS PROGRAM FOR SLICING.....
 
+str = "welcome everyone"
+
+print(str[1:4])
+print(str[0:])  #goes from 0 till end of string
+print(str[:12])
+
+# NEGATIVE INDEX (BACKWARD COUNTING )
+
+str = "apple"
+str[-3:-5]
+print(str[-3:-1])
+print(str[:]) #print entire string
+print(str[-5:-2])
