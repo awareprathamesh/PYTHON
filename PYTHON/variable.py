032589d8fbcb34d1701dbb766f09c067ec293d5c
awareprathamesh <prathameshaware58@gmail.com>
2026-06-06 10:56:19 +0530

    Update variable.py

--- PYTHON/variable.py
+++ PYTHON/variable.py
@@ -1,1 +1,8 @@
 
+name ="prathamesh" #string
+age = 21           # integer
+percentage  = 55.5       #floating value
+
+print( " my name is : " , name)
+print( " my age is : " , age)
+print( "  i  score " , percentage ,"%")
